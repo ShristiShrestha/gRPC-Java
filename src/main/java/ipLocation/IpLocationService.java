10fd34765c7dc6c1becc6bfe4ab027e693dc6ff8
Shristi Shrestha <sthashristi777@gmail.com>
2019-11-02 15:05:05 +0545

    ip-location-bloomrpc-tested-maxmind-geo-api

--- src/main/java/ipLocation/IpLocationService.java
+++ src/main/java/ipLocation/IpLocationService.java
@@ -39,7 +39,7 @@ public class IpLocationService extends ipToLocationGrpc.ipToLocationImplBase {
 
     @Override
     public void getMyLocationFromIp(IpToLocation.IpLocationRequest request, StreamObserver<IpToLocation.IpLocationResponse> responseObserver) {
-        System.out.println("Inside getMyLocationFromIp rpc/api of ipToLocation service in protobuf..");
+
         String ip =  request.getIp();
 
         // validate ip
@@ -72,14 +72,6 @@ public class IpLocationService extends ipToLocationGrpc.ipToLocationImplBase {
                 System.out.println("\n Country: " +country.getName()); // 'Nepal'
                 ipLocation.setCountryName(country.getName());
 
-                /*Subdivision subdivision = cityResponse.getMostSpecificSubdivision();
-                System.out.println("\n Subdivision: " + subdivision.getName());    // 'Minnesota'
-                System.out.println("\n Subdivision iso code: "+ subdivision.getIsoCode()); // 'MN'
-                ipLocation.setContinentName(continent.getName());
-
-                City city = cityResponse.getCity();
-                System.out.println("\n city: " + city.getName()); // 'Kathmandu' */
-
                 Location location = cityResponse.getLocation();
                 System.out.println(location.getLatitude());  // 44.9733
                 System.out.println(location.getLongitude()); // -93.2323
@@ -92,34 +84,6 @@ public class IpLocationService extends ipToLocationGrpc.ipToLocationImplBase {
             }catch(IOException | GeoIp2Exception e ){
                 System.out.print("Exception occured: " + e);
             }
-
-            /*
-            File file = new File("D:\\springboot\\Spring\\protobuf\\GeoLite2-City.mmdb");
-            System.out.println("\n file : " + file);
-            IpLocation ipLocation = null;
-            try{
-                ipLocation = new IpLocation();
-                LookupService lookup = new LookupService(file,LookupService.GEOIP_MEMORY_CACHE);
-                System.out.println("Hola: " + lookup+ "with ip: "+ ip);
-                Location locationServices = lookup.getLocation(ip); // problem
-                System.out.println("\n after getlocation of locationservices... \n");
-                ipLocation.setCountryName(locationServices.countryName);
-                ipLocation.setRegionName(regionName.regionNameByCode(
-                        locationServices.countryCode, locationServices.region));
-                ipLocation.setCityName(locationServices.city);
-                ipLocation.setLatitude(String.valueOf(locationServices.latitude));
-                ipLocation.setLongitude(String.valueOf(locationServices.longitude));
-                ipLocation.setIp(ip);
-                ipLocation.setType("IPV4");
-                System.out.println("My iplocation: " + ipLocation.toString());
-
-                response.setIp(ipLocation.getIp()).setType(ipLocation.getType()).setCountryName(ipLocation.getCountryName()).setRegionName(ipLocation.getRegionName()).setCity(ipLocation.getCityName()).setLatitude(ipLocation.getLatitude()).setLongitude(ipLocation.getLongitude()).setResponseCode(0);
-
-            }catch(IOException e){
-                    System.out.println("Exception caught " + e);
-                }
-
-             */
         }
         else{
             System.out.println("Ip not validated by server...");
